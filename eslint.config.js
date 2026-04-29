5e1c80725f644477ec0326daf6bc73a4ab0e75d3
Nicolás <147382015+yust3c@users.noreply.github.com>
2026-04-29 17:27:35 +0200

    Add ESLint configuration file

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.js
+module.exports = [
+    {
+        "rules": {
+            "no-cond-assign": [
+                "error",
+                "always"
+            ],
+            "indent": [
+                "error",
+                2
+            ],
+            "linebreak-style": [
+                "error",
+                "unix"
+            ],
+            "quotes": [
+                "error",
+                "single"
+            ],
+            "no-unused-vars": "off",
+            "semi": [
+                "error",
+                "always"
+            ]
+        }
+    }
+];
